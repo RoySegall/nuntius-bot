b9a2b2ba22ecb8efc563bb51f23f5737b2d95df3
Roy Segall <roy.segall@gizra.com>
2017-05-26 13:26:10 +0300

    Start working on the DB metadata.

--- src/Db/DbMetadataHandlerInterface.php
+++ src/Db/DbMetadataHandlerInterface.php
@@ -7,4 +7,29 @@ namespace Nuntius\Db;
  */
 interface DbMetadataHandlerInterface {
 
+  /**
+   * return the DB type - SQL or NoSQL
+   *
+   * @return string
+   *   The DB type.
+   */
+  public function dbType();
+
+  /**
+   * Return a small description when choosing the DB at the install phase.
+   *
+   * @return string
+   *   A small description about the DB.
+   */
+  public function installerDescription();
+
+  /**
+   * Return true or false of the DB support real time.
+   *
+   * We need to know this for the live view task.
+   *
+   * @return bool
+   */
+  public function supportRealTime();
+
 }
--- src/Db/RethinkDB/RethinkDbMetadataHandler.php
+++ src/Db/RethinkDB/RethinkDbMetadataHandler.php
@@ -9,4 +9,25 @@ use Nuntius\Db\DbMetadataHandlerInterface;
  */
 class RethinkDbMetadataHandler implements DbMetadataHandlerInterface {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function dbType() {
+    return 'NoSQL';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function installerDescription() {
+    return 'No SQL light weight DB with real time support.';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function supportRealTime() {
+    return TRUE;
+  }
+
 }
