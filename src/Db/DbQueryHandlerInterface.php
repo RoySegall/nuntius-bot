7e23bc54d650b5fc36f3c990ef5869b1a3753a38
Roy Segall <roy.segall@gizra.com>
2017-05-26 16:02:52 +0300

    Adding the query handler.

--- src/Db/DbQueryHandlerInterface.php
+++ src/Db/DbQueryHandlerInterface.php
@@ -7,4 +7,36 @@ namespace Nuntius\Db;
  */
 interface DbQueryHandlerInterface {
 
+  /**
+   * Set the table name.
+   *
+   * @param $table
+   *   The table name.
+   *
+   * @return \Nuntius\Db\DbQueryHandlerInterface
+   */
+  public function table($table);
+
+  /**
+   * Set the conditions of the query.
+   *
+   * @param $property
+   *   The field.
+   * @param $value
+   *   The value.
+   * @param $operator
+   *   The operator: <, >, =>, >=, !=, =, IN, NOT_IN. Default set to =.
+   *
+   * @return DbQueryHandlerInterface
+   */
+  public function condition($property, $value, $operator = '=');
+
+  /**
+   * Execute the query.
+   *
+   * @return array
+   *   List of IDs.
+   */
+  public function execute();
+
 }
--- src/Db/RethinkDB/RethinkDbQueryHandler.php
+++ src/Db/RethinkDB/RethinkDbQueryHandler.php
@@ -3,10 +3,68 @@
 namespace Nuntius\Db\RethinkDB;
 
 use Nuntius\Db\DbQueryHandlerInterface;
+use Nuntius\Nuntius;
 
 /**
  * RethinkDB query handler.
  */
 class RethinkDbQueryHandler implements DbQueryHandlerInterface {
+
+  /**
+   * RethinkDB service.
+   *
+   * @var \Nuntius\NuntiusRethinkdb
+   */
+  protected $rethinkDB;
+
+  /**
+   * The table name.
+   *
+   * @var string
+   */
+  protected $table;
+
+  /**
+   * List of conditions.
+   *
+   * @var array
+   */
+  protected $conditions = [];
+
+  /**
+   * Constructing the query.
+   */
+  function __construct() {
+    $this->rethinkDB = Nuntius::getRethinkDB();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function table($table) {
+    $this->table = $table;
+
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function condition($property, $value, $operator = '=') {
+    $this->conditions[] = [
+      'property' => $property,
+      'value' => $value,
+      'operator' => $operator,
+    ];
+
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute() {
+
+  }
 
 }
