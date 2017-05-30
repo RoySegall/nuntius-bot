069027934a11a4b84e7a7ab9791689241a908bc0
Roy Segall <roy.segall@gizra.com>
2017-05-30 17:43:38 +0300

    Adding operations to the dispatcher.

--- src/Db/DbOperationHandlerInterface.php
+++ src/Db/DbOperationHandlerInterface.php
@@ -7,4 +7,108 @@ namespace Nuntius\Db;
  */
 interface DbOperationHandlerInterface {
 
+  /**
+   * Create the DB.
+   *
+   * @param string $db
+   *
+   * @return \Nuntius\Db\DbOperationHandlerInterface
+   */
+  public function dbCreate($db);
+
+  /**
+   * Deleting the DB.
+   *
+   * @param string $db
+   *   The DB name.
+   *
+   * @return \Nuntius\Db\DbOperationHandlerInterface
+   */
+  public function dbDrop($db);
+
+  /**
+   * Return all the list of the DBs.
+   *
+   * @return array
+   */
+  public function dbList();
+
+  /**
+   * Check if the DB exists.
+   *
+   * @param string $db
+   *   The DB name.
+   *
+   * @return bool
+   */
+  public function dbExists($db);
+
+  /**
+   * Create a table.
+   *
+   * @param string $table
+   *   The table name.
+   *
+   * @return \Nuntius\Db\DbOperationHandlerInterface
+   */
+  public function tableCreate($table);
+
+  /**
+   * Delete the table.
+   *
+   * @param string $table
+   *   The table name.
+   *
+   * @return \Nuntius\Db\DbOperationHandlerInterface
+   */
+  public function tableDrop($table);
+
+  /**
+   * Return list of tables.
+   *
+   * @return bool
+   */
+  public function tableList();
+
+  /**
+   * Check if the table exists.
+   *
+   * @param string $table
+   *   The table name.
+   *
+   * @return bool
+   */
+  public function tableExists($table);
+
+  /**
+   * Create an index in a table.
+   *
+   * @param string $column
+   * @param string $table
+   *
+   * @return \Nuntius\Db\DbOperationHandlerInterface
+   */
+  public function indexCreate($column, $table);
+
+  /**
+   * Drop the index.
+   *
+   * @param string $column
+   * @param string $table
+   *
+   * @return \Nuntius\Db\DbOperationHandlerInterface
+   */
+  public function indexDrop($column, $table);
+
+  /**
+   * Get all the index in a table.
+   *
+   * @param string $column
+   * @param string $table
+   *
+   * @return array
+   *   The list of the indexes.
+   */
+  public function indexList($column, $table);
+
 }
