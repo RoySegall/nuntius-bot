2cef5ee8682e7d69ad4cb9391a25cef376a5f587
Roy Segall <roy.segall@gizra.com>
2017-06-02 00:31:41 +0300

    Try to comment out.

--- src/Db/RethinkDB/RethinkDbQueryHandler.php
+++ src/Db/RethinkDB/RethinkDbQueryHandler.php
@@ -194,7 +194,7 @@ class RethinkDbQueryHandler implements DbQueryHandlerInterface {
     foreach ($query->run($this->rethinkDB->getConnection()) as $item) {
       $item_copy = $item->getArrayCopy();
 
-      $this->processSubArrays($item_copy);
+//      $this->processSubArrays($item_copy);
       $items[] = $item_copy;
     }
 
