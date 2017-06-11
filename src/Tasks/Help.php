d9da4564f0546203ac1d341cea1156b594005275
Roy Segall <roy.segall@gizra.com>
2017-06-11 23:26:13 +0300

    Sending a button :)

--- src/Tasks/Help.php
+++ src/Tasks/Help.php
@@ -5,6 +5,9 @@ namespace Nuntius\Tasks;
 use Nuntius\Nuntius;
 use Nuntius\TaskBaseAbstract;
 use Nuntius\TaskBaseInterface;
+use pimax\FbBotApp;
+use pimax\Menu\LocalizedMenu;
+use pimax\Menu\MenuItem;
 
 /**
  * Remind to the user something to do.
@@ -52,7 +55,34 @@ class Help extends TaskBaseAbstract implements TaskBaseInterface {
    * A Facebook only text.
    */
   public function facebookListOfScopes() {
-    return 'a';
+    $task_manager = Nuntius::getTasksManager();
+
+    $buttons = [];
+
+    foreach ($task_manager->getTasks() as $task_id => $task) {
+      if ($task_id == 'help') {
+        continue;
+      }
+
+      foreach ($task->scope() as $scope) {
+        $buttons[] = [
+          'type' => 'postback',
+          'title' => $scope['human_command'],
+          'payload' => $scope['human_command'],
+        ];
+      }
+    }
+
+    return [
+      'attachment' => [
+        'type' => 'template',
+        'payload' => [
+          'template_type' => 'button',
+          'text' => 'What do you want to do next?',
+          'buttons' => array_slice($buttons, 0, 3),
+        ],
+      ],
+    ];
   }
 
 }
