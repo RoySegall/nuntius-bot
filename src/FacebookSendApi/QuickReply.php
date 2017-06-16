9f35bdaefc1eb4cac1cefc8b46afa2df3f03cd84
Roy Segall <roy.segall@gizra.com>
2017-06-16 15:55:06 +0300

    Done with quick reply.

--- src/FacebookSendApi/QuickReply.php
+++ src/FacebookSendApi/QuickReply.php
@@ -7,20 +7,60 @@ namespace Nuntius\FacebookSendApi;
  */
 class QuickReply extends SendAPITransform {
 
+  /**
+   * The content type.
+   *
+   * @param $content_type
+   *   The content type.
+   *
+   * @return $this
+   */
   public function contentType($content_type) {
+    $this->data['content_type'] = $content_type;
 
+    return $this;
   }
 
+  /**
+   * Set the title.
+   *
+   * @param $title
+   *   The title.
+   *
+   * @return $this
+   */
   public function title($title) {
+    $this->data['title'] = $title;
 
+    return $this;
   }
 
+  /**
+   * Set the payload.
+   *
+   * @param $payload
+   *   The payload.
+   *
+   * @return $this
+   */
   public function payload($payload) {
+    $this->data['payload'] = $payload;
 
+    return $this;
   }
 
+  /**
+   * Set the image URL.
+   *
+   * @param $image_url
+   *   The image URL.
+   *
+   * @return $this
+   */
   public function imageUrl($image_url) {
+    $this->data['image_url'] = $image_url;
 
+    return $this;
   }
 
 }
--- text.php
+++ text.php
@@ -5,24 +5,9 @@ require_once 'vendor/autoload.php';
 $facebook = new \Nuntius\FacebookSendApi\SendAPI();
 
 $buttons_template = $facebook
-  ->templates
-  ->airlineFlightUpdate;
-
-$buttons_template
-  ->introMessage('You ticket')
-  ->updateType('CANCELLATION')
-  ->locale('he_IL')
-  ->themeColor('#ff9900')
-  ->pnrNumber('119900')
-  ->updateFlightInfo(
-    $facebook->templates->updateFlightInfo
-      ->flightNumber('190')
-      ->departureAirport($facebook->templates->airport->airportCode('NATBAG')->city('TLV')->terminal(3)->gate(20))
-      ->arrivalAirport($facebook->templates->airport->airportCode('ROME')->city('Rome')->terminal(2)->gate('6D'))
-      ->boardingTime('2015-12-26T10:30')
-      ->departureTime('2015-12-26T13:30')
-      ->arrivalTime('2015-12-27T10:30')
-  );
+  ->quickReplies
+  ->text('Select a color')
+  ->addQuickReply($facebook->quickReply->contentType('location'));
 
 $options = [
   'form_params' => [
