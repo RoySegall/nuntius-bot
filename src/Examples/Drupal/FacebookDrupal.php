7cc17cf10ac8ced3188fa48876c3bb8eb0af435f
Roy Segall <roy.segall@gizra.com>
2017-06-26 23:43:18 +0300

    First sketch for sending to the user a message.

--- src/Examples/Drupal/FacebookDrupal.php
+++ src/Examples/Drupal/FacebookDrupal.php
@@ -3,6 +3,7 @@
 namespace Nuntius\WebhooksRounting;
 
 use Nuntius\Examples\Drupal\DrupalBaseWebhook;
+use Nuntius\Nuntius;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -17,6 +18,31 @@ class FacebookDrupal extends DrupalBaseWebhook {
    * {@inheritdoc}
    */
   protected function trigger() {
+    $subtitle = !empty($this->payload->body->und[0]->value) ? $this->payload->body->und[0]->value : '';
+
+    // Look for registered users from the given URL.
+    $users = [];
+
+    // Prepare the send API object.
+    $send_api = \Nuntius\Nuntius::facebookSendApi();
+    $send_api->setAccessToken(Nuntius::getSettings()->getSetting('fb_token'));
+
+    $element = $send_api->templates->element;
+    $payload = $send_api->templates->generic
+      ->addElement(
+        $element
+          ->title($this->payload->title)
+          ->subtitle($subtitle)
+          ->addButton($send_api->buttons->url->title('Take me there!')->url($this->url))
+      );
+
+    // Loop over the users.
+    foreach ($users as $user) {
+      $send_api
+        ->setRecipientId($user->recpient_id)
+        ->sendMessage($payload);
+    }
+
     return new Response();
   }
 
