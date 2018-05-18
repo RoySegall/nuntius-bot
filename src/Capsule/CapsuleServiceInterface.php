ff9e43f6834207f21af3c30a2a700da317bcd7fd
Roy Segall <roy@segall.io>
2018-05-18 12:39:28 +0300

    More documentation.

--- src/Capsule/CapsuleServiceInterface.php
+++ src/Capsule/CapsuleServiceInterface.php
@@ -23,18 +23,6 @@ interface CapsuleServiceInterface {
   public function __construct(Finder $finder, DbDispatcher $dbDispatcher);
 
   /**
-   * Search for all the capsules in the system.
-   *
-   * Capsules can be located in three folders:
-   *  - core: All the capsules which provided by nuntius.
-   *  - contrib: All the capsules which downloaded by composer.
-   *  - custom: Capsules which written by the user.
-   *
-   * @return mixed
-   */
-  public function getCapsules();
-
-  /**
    * Get the root folder.
    *
    * @return string
@@ -53,6 +41,18 @@ interface CapsuleServiceInterface {
   public function setRoot($root);
 
   /**
+   * Search for all the capsules in the system.
+   *
+   * Capsules can be located in three folders:
+   *  - core: All the capsules which provided by nuntius.
+   *  - contrib: All the capsules which downloaded by composer.
+   *  - custom: Capsules which written by the user.
+   *
+   * @return mixed
+   */
+  public function getCapsules();
+
+  /**
    * Enabling a capsule.
    *
    * @param string $capsule_name
@@ -66,36 +66,56 @@ interface CapsuleServiceInterface {
   /**
    * Disable capsule.
    * 
-   * @param $capsule_name
+   * @param string $capsule_name
+   *  The capsule name.
    *
    * @return mixed
    */
   public function disableCapsule($capsule_name);
 
   /**
-   * @param $mode
+   * Get a list of the capsules.
    *
-   * @return mixed
+   * @param string $mode
+   *  Two options are allowed:
+   *    - 'enabled': Get all the enabled capsules.
+   *    - 'disabled': Get all the disabled capsules.
+   *
+   * @return array
+   *  List of capsules.
    */
   public function capsuleList($mode);
 
   /**
-   * @param $capsule_name
-   * @param $implementation_type
+   * Get the implementations of the capsules from the services.yml file.
+   *
+   * @param string $capsule_name
+   *  The capsule name.
+   * @param string $implementation_type
+   *  The implementation type in the services.yml file of the capsule.s
    *
    * @return mixed
    */
   public function getCapsuleImplementations($capsule_name, $implementation_type);
 
   /**
-   * @param $capsule_name
+   * Checking if the capsule exists in the file system.
+   *
+   * @param string $capsule_name
+   *  The name of capsule.
    *
    * @return mixed
    */
   public function capsuleExists($capsule_name);
 
   /**
-   * @param $capsule_name
+   * Checking if the capsule enabled
+   *
+   * Unlike self::capsuleExists which check if the capsule exists in the file
+   * system, this will check if the capsule enabled in the system.
+   *
+   * @param string $capsule_name
+   *  The capsule name.
    *
    * @return mixed
    */
