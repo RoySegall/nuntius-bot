3e3024702cb59913918e01fafa9870c53c3bd2df
Roy Segall <roy@segall.io>
2018-05-19 00:26:53 +0300

    Pushing boilerplate for the capsule tet.

---
+++ tests/CapsuleServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace tests;
+
+use Nuntius\Examples\Names\NameEvent;
+use Nuntius\Nuntius;
+
+/**
+ * Testing event dispatcher.
+ */
+class CapsuleServiceTest extends TestsAbstract {
+
+  /**
+   * Testing the capsule scraper.
+   */
+  public function testGetCapsules() {
+  }
+
+  public function testEnableCapsule() {
+
+  }
+
+  public function testDisableCapsule() {
+
+  }
+
+  public function testCapsuleList() {
+
+  }
+
+  public function testGetCapsuleImplementations() {
+
+  }
+
+  public function testCapsuleExists() {
+
+  }
+
+  public function testCapsuleEnabled() {
+
+  }
+
+}
