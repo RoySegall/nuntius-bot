ec8fae2796cefa7cc061f769254731bf29ee1872
Roy Segall <roy@segall.io>
2018-05-20 15:16:25 +0300

    Fixing the rethnkdb.

--- autoloader.php
+++ autoloader.php
@@ -7,23 +7,27 @@
 /** @var \Composer\Autoload\ClassLoader $composer */
 $composer = require_once 'vendor/autoload.php';
 
-$capsule_manager = \Nuntius\Nuntius::getCapsuleManager();
+try {
+  $capsule_manager = \Nuntius\Nuntius::getCapsuleManager();
 
-// Get the enabled capsules.
-$enabled = $capsule_manager->capsuleList('enabled');
+  // Get the enabled capsules.
+  $enabled = $capsule_manager->capsuleList('enabled');
 
-// Get all the capsules.
-$capsules = $capsule_manager->getCapsules();
+  // Get all the capsules.
+  $capsules = $capsule_manager->getCapsules();
 
-foreach ($enabled as $enable) {
-  $capsule = $capsules[$enable];
-  $path = $capsules[$enable];
-  $names = explode('_', $capsule['machine_name']);
-  $namespace = 'Nuntius\\' . implode('', array_map(function($item) {
-    return ucfirst($item);
-  }, $names));
+  foreach ($enabled as $enable) {
+    $capsule = $capsules[$enable];
+    $path = $capsules[$enable];
+    $names = explode('_', $capsule['machine_name']);
+    $namespace = 'Nuntius\\' . implode('', array_map(function($item) {
+        return ucfirst($item);
+      }, $names));
 
-  $composer->addPsr4($namespace . '\\', $capsule_manager->getRoot() . '/capsules/core/system/src/');
+    $composer->addPsr4($namespace . '\\', $capsule_manager->getRoot() . '/capsules/core/system/src/');
+  }
+} catch (Exception $e) {
+  
 }
 
 return $composer;
