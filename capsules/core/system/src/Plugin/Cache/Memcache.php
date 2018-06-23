68b5fe196dff8ac09c393c91a2cd0fde31898542
Roy Segall <roy@segall.io>
2018-06-23 19:51:34 +0300

    Starting to work on memcache.

--- capsules/core/system/src/Plugin/Cache/Memcache.php
+++ capsules/core/system/src/Plugin/Cache/Memcache.php
@@ -6,18 +6,51 @@ use Nuntius\Nuntius;
 use Nuntius\NuntiusConfig;
 use Nuntius\System\CacheBase;
 use Nuntius\System\Annotations\Cache as cache;
+use Nuntius\System\HookContainerInterface;
 
 /**
  * @cache(
  *   id = "memcache",
  * )
  */
-class Memcache extends CacheBase {
+class Memcache extends CacheBase implements HookContainerInterface {
+
+  /**
+   * @var NuntiusConfig
+   */
+  protected $config;
+
+  /**
+   * @var \Memcached
+   */
+  protected $memcached;
+
+  /**
+   * {@inheritdoc}
+   */
+  static function getContainer(\Symfony\Component\DependencyInjection\ContainerBuilder $container) {
+    return new static($container->get('config'));
+  }
+
+  /**
+   * Memcache constructor.
+   *
+   * @param NuntiusConfig $config
+   */
+  public function __construct(NuntiusConfig $config) {
+    $this->config = $config;
+
+    $connection = $config->getSetting('memcache');
+
+    $this->memcached = new \Memcached();
+    $this->memcached->addServer($connection['host'], $connection['port']);
+  }
 
   /**
    * {@inheritdoc}
    */
   public function install() {
+    // Nothing to do here.
   }
 
   /**
@@ -35,6 +68,7 @@ class Memcache extends CacheBase {
    * @return mixed
    */
   public function get($cid) {
+    return $this->memcached->get($cid);
   }
 
   /**
@@ -47,6 +81,17 @@ class Memcache extends CacheBase {
    * {@inheritdoc}
    */
   public function set($id, $content, $expires) {
+
+    $old_content = $content;
+    $content = serialize($content);
+
+    $this->memcached->set($id, $content, $expires);
+
+    return [
+      'id' => $id,
+      'content' => $old_content,
+      'expires' => $expires,
+    ];
   }
 
   /**
