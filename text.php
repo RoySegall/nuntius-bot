d56971d6536a34ca88ee1dda16ef2f685c09a5d9
Roy Segall <roy@segall.io>
2018-06-29 10:19:09 +0300

    Benchmarking.

--- text.php
+++ text.php
@@ -2,36 +2,38 @@
 
 require_once 'autoloader.php';
 
+/**
+ * @param $type
+ * @return \Nuntius\System\CacheBase
+ * @throws \Nuntius\Capsule\CapsuleErrorException
+ */
+function get_cache($type) {
+    return \Nuntius\System\System::getCacheManager()->createInstance($type);
+}
+
+function get_entity() {
+    $entity = \Nuntius\System\System::getEntityManager()->createInstance('system');
+    return $entity->loadMultiple(['0161dd3c-b8af-4d84-b1cf-114c5162363e', '0e3512e1-e02c-45a8-bfb3-cc5a24eb40d9']);
+}
+
+
+function benchmark1() {
+    $cache = get_cache('memcache');
+    $cache->get('list_entities');
+}
+
+function benchmark2() {
+    $cache = get_cache('db');
+    $cache->get('list_entities');
+}
+
 $time_start = microtime(true);
-
-try {
-
-//  \Nuntius\Nuntius::getCapsuleManager()->enableCapsule('system');
-
-  /** @var \Nuntius\System\Plugin\Cache\Memcache $cache */
-  $cache = \Nuntius\System\System::getCacheManager()->createInstance('memcache');
-
-  /** @var \Nuntius\System\Plugin\Entity\System $entity */
-  $entity = \Nuntius\System\System::getEntityManager()->createInstance('system');
-
-//  $cache->set('a', $entity->loadMultiple());
-//
-  $cache->get('a');
-
-
-//  if (!$cache_results = $cache->get('entities')) {
-//    $cache_results = $cache->set('entities', $entity->loadMultiple());
-//  } else {
-//    $cache_results['data'] = $entity->loadMultiple();
-//  }
-
-//  $cache->clear();
-} catch (Exception $e) {
-  d($e->getMessage());
+for ($i = 0; $i < 1000; $i++) {
+    benchmark1();
 }
 
 $time_end = microtime(true);
 
-$execution_time = ($time_end - $time_start)/60;
+$execution_time = ($time_end - $time_start);
 
 echo "The time of execution is: {$execution_time}\n";
