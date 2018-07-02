c01c406bdb833c430b96029641fb9c8c91953b31
Roy Segall <roy@segall.io>
2018-07-02 18:49:02 +0300

    Setting up the memecache

--- text.php
+++ text.php
@@ -3,6 +3,14 @@
 require_once 'autoloader.php';
 
 /** @var \Nuntius\System\Plugin\Cache\Memcache $cache */
-$cache = \Nuntius\System\System::getCacheManager()->createInstance('memcache');
+//$cache = \Nuntius\System\System::getCacheManager()->createInstance('memcache');
+//
+//d($cache->get('a'));
 
-d($cache->get('a'));
+$foo = \Nuntius\System\System::getEntityManager()->createInstance('system')->load('0161dd3c-b8af-4d84-b1cf-114c5162363e');
+
+$ser = serialize($foo);
+
+$unser = unserialize($ser);
+
+d($unser);
